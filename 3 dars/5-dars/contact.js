28fa0a6065c7b900123952b10283e31d93b808d4
saparbayevabror72-max <saparbayevabror72@gmail.com>
2025-10-16 15:44:49 +0500

    Delete 3 dars directory

--- 3 dars/5-dars/contact.js
+++
@@ -1,6 +0,0 @@
-let ismlar = ['Abror','Akbarmirzo','Xudoyberdi','iskandar','Bekzod','Muhammadjon','Raximboy','Bekturdi','Muxammadjon','Bekzod','Oybek']
-function joshadonniTop() {
-    let tasodifiSon = Math.floor(Math.random() * ismlar.length)
-    alert(ismlar[tasodifiSon])
-}
-joshadonniTop()
--- 3 dars/6-dars/1/index.js
+++
@@ -1,9 +0,0 @@
-let n = Math.random()
-console.log(n)
-
-let oltigacha = n * 11
-let yahlitlangan = Math.floor(oltigacha)
-let dogriYahlit = Math.round(oltigacha)
-console.log(oltigacha)
-console.log(yahlitlangan)
-console.log(dogriYahlit)
