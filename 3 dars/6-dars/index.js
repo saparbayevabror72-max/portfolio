b8b8533f25627c00b68ea41a5ae0567c82b681c5
saparbayevabror72-max <saparbayevabror72@gmail.com>
2025-10-16 15:38:56 +0500

    Add files via upload

---
+++ 3 dars/6-dars/index.js
@@ -0,0 +1,12 @@
+// let cat = prompt('Mushugingizni yoshini kiriting')
+
+// console.log (cat)
+
+// alert(16+4*cat)
+
+ let ism = prompt('ismingizni kiritinh') 
+ let a = ism.slice(0,1)
+ let b = ism.slice(1)
+ a=a.toUpperCase()
+ b=b.toLowerCase()
+ alert(a+b)
