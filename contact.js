bc4cce515ae8c1f28bb19805279cdbded2515543
saparbayevabror72-max <saparbayevabror72@gmail.com>
2025-10-16 15:45:57 +0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,6 @@
+let ismlar = ['Abror','Akbarmirzo','Xudoyberdi','iskandar','Bekzod','Muhammadjon','Raximboy','Bekturdi','Muxammadjon','Bekzod','Oybek']
+function joshadonniTop() {
+    let tasodifiSon = Math.floor(Math.random() * ismlar.length)
+    alert(ismlar[tasodifiSon])
+}
+joshadonniTop()
